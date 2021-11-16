f446a838c22117f5ef13fbf1210828b154fe30de
Dennis Z <zdennisz@gmail.com>
2021-11-16 12:30:40 +0200

    added basic modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import "./App.css";
 import TvShowName from "./components/TvShowName/TvShowName";
 import useKeyPress from "./components/hooks/useKeyPress";
 import Hint from "./components/Hint/Hint";
+import Statistics from "./components/Statistics/Statistics";
 function App() {
 	const keyPress = useKeyPress();
 	const [lifePoints, setLifePoints] = useState(3);
+	const [score, setScore] = useState(0);
 	const isPartOfWord = "Henry";
 	const [showHint, setShowHint] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+	const [hintPressedAmount, setHitPressedAmount] = useState(0);
+
 	const checkGuessHandler = () => {
 		if (lifePoints > 0 && !isPartOfWord.includes(keyPress.key)) {
 			setLifePoints((state) => state - 1);
@@ -19,15 +24,26 @@ function App() {
 	const showHintHandler = () => {
 		setShowHint((state) => !state);
 	};
+	const showStatisticsHandler = () => {
+		setShowModal((state) => !state);
+	};
 	return (
 		<div className='App'>
 			{lifePoints}
 			<TvShowName tvShowName={"Henry"} />
-			<div className=''>
+			<div>
 				<button onClick={checkGuessHandler}>Check the guess</button>
 				<button onClick={showHintHandler}>Hint</button>
 				{showHint && <Hint hint={"bla bla some hint"} />}
-				<button>Statistics</button>
+				{showModal && (
+					<Statistics
+						amountOfRightGusses={score}
+						amountOfWrongGusses={lifePoints}
+						amountOftimesPressedHint={hintPressedAmount}
+						onClose={showStatisticsHandler}
+					/>
+				)}
+				<button onClick={showStatisticsHandler}>Statistics</button>
 			</div>
 		</div>
 	);
---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import "./Statistics.css";
+declare interface StatisticsProps {
+	amountOfRightGusses: number;
+	amountOfWrongGusses: number;
+	amountOftimesPressedHint: number;
+	onClose: () => void;
+}
+
+const Statistics = ({
+	amountOfRightGusses,
+	amountOfWrongGusses,
+	amountOftimesPressedHint,
+	onClose,
+}: StatisticsProps) => {
+	const closeOnEscapeKeyDown = (e: any) => {
+		if ((e.charCode || e.keyCode) === 27) {
+			onClose();
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("keydown", closeOnEscapeKeyDown);
+		return () => {
+			window.removeEventListener("keydown", closeOnEscapeKeyDown);
+		};
+	}, []);
+
+	return (
+		<div className='modal'>
+			<div className='modal_content'>
+				<div className='modal-header'>Statistics</div>
+				<div className='modal_body'>
+					{`  The amount of right guesses is ${amountOfRightGusses} 
+                    	The amount of wrong guesses is ${amountOfWrongGusses} 
+                    	Amount of times pressed on hint button ${amountOftimesPressedHint}`}
+				</div>
+				<div className='modal_footer'>
+					<button className='button' onClick={onClose}>
+						Close
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Statistics;
