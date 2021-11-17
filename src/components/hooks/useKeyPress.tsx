89e51b7894f2d642e1c4dc7b1ea5ee5f69f96d4c
Dennis Z <zdennisz@gmail.com>
2021-11-17 17:44:46 +0200

    added key codes to support more chars

--- src/components/hooks/useKeyPress.tsx
+++ src/components/hooks/useKeyPress.tsx
@@ -9,10 +9,18 @@ const useKeyPress = (): useKeyPressProps => {
 
 	useEffect(() => {
 		const upHandler = (e: any) => {
+			console.log("e", e);
 			if (
 				(e.keyCode >= 65 && e.keyCode <= 90) ||
 				e.keyCode === 8 ||
-				e.keyCode === 32
+				e.keyCode === 186 ||
+				e.keyCode === 188 ||
+				e.keyCode === 189 ||
+				e.keyCode === 190 ||
+				e.keyCode === 222 ||
+				e.keyCode === 32 ||
+				(e.keyCode >= 48 && e.keyCode <= 57) ||
+				(e.keyCode >= 96 && e.keyCode <= 105)
 			) {
 				setKeyPressed({ key: e.key });
 			}
