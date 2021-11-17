f028d76e02935d1ac4fcee7f4cddf280e12af3bb
Dennis Z <zdennisz@gmail.com>
2021-11-17 14:07:31 +0200

    removed temp fix for env variables

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { REACT_APP_MOVIE_API_KEY } from "./keys";
 export const getMovies = async (pagenumber: number) => {
 	try {
 		const movies = await axios.get(
-			`https://api.themoviedb.org/3/tv/top_rated?api_key=${REACT_APP_MOVIE_API_KEY}&page=${pagenumber}`
+			`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&page=${pagenumber}`
 		);
 		return movies.data.results;
 	} catch (err) {
