3a3da69a968e37f7cba6e68d362a067ccaa3f1e4
Dennis Z <zdennisz@gmail.com>
2021-11-18 18:16:00 +0200

    added media query support

--- src/components/Hint/Hint.tsx
+++ src/components/Hint/Hint.tsx
@@ -8,7 +8,7 @@ const Hint = ({ hint }: HintProps) => {
 	return (
 		<div className='hint_container'>
 			<i className='fas fa-question-circle hint_icon' />
-			<div>{hint}</div>
+			<div className='hint_text'>{hint}</div>
 		</div>
 	);
 };
